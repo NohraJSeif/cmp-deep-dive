56f5688ed0a664890d89b00edc301a22ed65f44a
NohraJSeif <nseif89@hotmail.com>
2025-08-23 19:01:54 -0400

    cleanup and adding host to css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
+import { SeverStatusComponent } from './dashboard/sever-status/sever-status.component';
+import { TrafficComponent } from './dashboard/traffic/traffic.component';
+import { TicketsComponent } from './dashboard/tickets/tickets.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, DashboardComponent],
+  imports: [
+    HeaderComponent,
+    DashboardItemComponent,
+    SeverStatusComponent,
+    TrafficComponent,
+    TicketsComponent,
+  ],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
