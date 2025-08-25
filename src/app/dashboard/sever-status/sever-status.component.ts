acf47b06c0a26f762d45cca7ad90c0715101d4de
NohraJSeif <nseif89@hotmail.com>
2025-08-24 22:58:09 -0400

    ngOnInit

--- src/app/dashboard/sever-status/sever-status.component.ts
+++ src/app/dashboard/sever-status/sever-status.component.ts
@@ -8,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './sever-status.component.css',
 })
 export class SeverStatusComponent {
-  currentStatus = 'online';
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+
+  ngOnInit() {
+    setInterval(() => {
+      const rnd = Math.random();
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
 }
